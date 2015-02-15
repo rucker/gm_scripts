411a923b583aa7d4bd62d1060a5ae1daaeaf393e
rucker <rick.ucker@gmail.com>
2015-02-15 12:47:27 -0500

    Allow only on /overview.event to avoid breakages. Format includes.

--- Mint_Module_Hider/Mint_Module_Hider.user.js
+++ Mint_Module_Hider/Mint_Module_Hider.user.js
@@ -2,9 +2,9 @@
 // @name        Mint Module Hider
 // @namespace   rucker@github
 // @description Mint Module Hider
-// @include     https://wwws.mint.com/*
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @include     https://wwws.mint.com/overview.event
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @version     2
 // @grant       none
 waitForKeyElements('#module-advice', setStyle);
