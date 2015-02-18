fccc2b80b9c9cab90b6e059262ecf1b29bb76f75
rucker <rick.ucker@gmail.com>
2015-02-17 21:46:51 -0500

    Allow script on /*

--- Mint_Module_Hider/Mint_Module_Hider.user.js
+++ Mint_Module_Hider/Mint_Module_Hider.user.js
@@ -2,7 +2,7 @@
 // @name        Mint Module Hider
 // @namespace   rucker@github
 // @description Mint Module Hider
-// @include     https://wwws.mint.com/overview.event
+// @include     https://wwws.mint.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @version     2
