751cbca5d57e03ea7de14a53640f817335482e8f
Rick Ucker <rick.ucker@gmail.com>
2016-01-16 14:06:10 -0500

    V3 rewrite. Remove offending nodes instead of hiding.

--- Mint_Module_Hider/Mint_Module_Hider.user.js
+++ Mint_Module_Hider/Mint_Module_Hider.user.js
@@ -2,18 +2,18 @@
 // @name        Mint Module Hider
 // @namespace   rucker@github
 // @description Mint Module Hider
-// @include     https://wwws.mint.com/*
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @include     https://wwws.mint.com/overview.event
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version     2
-// @grant       none
-this.$ = this.jQuery = jQuery.noConflict(true);
-waitForKeyElements('#module-advice', setStyle);
-waitForKeyElements('#module-credit-score', setStyle);
-function setStyle(jNode) {
-  if (!jNode) {
-    return;
-  }
-  jNode.css('display', 'none');
-}
+// @run-at      document-idle
+// @version     3
 // ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+var nodes = ['.AdviceView', '.CreditScoreView']
+$.each(nodes, function(index, value) {
+  waitForKeyElements(value, function() {
+    $(value).remove();
+  });
+});
+
