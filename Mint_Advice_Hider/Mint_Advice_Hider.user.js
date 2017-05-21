e58689cae9ad0f788a26d05428ab91df56d4828c
Rick Ucker <rick.ucker@gmail.com>
2017-05-21 13:38:31 -0400

    Update class names and @include domain

--- Mint_Advice_Hider/Mint_Advice_Hider.user.js
+++ Mint_Advice_Hider/Mint_Advice_Hider.user.js
@@ -2,17 +2,17 @@
 // @name        Mint Advice Hider
 // @namespace   rucker@github
 // @description This script will hide certain screen-hogging elements in the mint.com UI.
-// @include     https://wwws.mint.com/*
+// @include     https://mint.intuit.com/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @run-at      document-idle
 // @grant	none
 // @updateURL	https://raw.githubusercontent.com/rucker/gm_scripts/master/Mint_Advice_Hider/Mint_Advice_Hider.user.js
-// @version     3.0.1
+// @version     3.1.1
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
-var nodes = ['.AdviceView', '.CreditScoreView']
+var nodes = ['.adviceWidget', '.creditScoreWidget']
 $.each(nodes, function(index, value) {
   waitForKeyElements(value, function() {
     $(value).remove();
